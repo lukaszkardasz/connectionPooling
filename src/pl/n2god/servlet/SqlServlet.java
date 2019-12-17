3ee94513f36879071c11ee4a7efeb1a1dec06d62
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-12-17 19:52:20 +0100

    connection singleton dbUtil

--- src/pl/n2god/servlet/SqlServlet.java
+++ src/pl/n2god/servlet/SqlServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.transform.Result;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
